74cb15740812e8f14fd6f5dd29d0c2125119681e
Julius <julius.robinson95@yahoo.com>
2017-08-28 14:39:56 -0500

    added files

---
+++ before & after/script.js
@@ -0,0 +1,39 @@
+// var playerOne = document.getElementById('player1');
+// var playerTwo = document.getElementById('player2');
+//
+//
+// playerOne.addEventListener('click', name);
+// playerTwo.addEventListener('click', name);
+//
+//
+//
+// function name() {
+//   var para = this.attributes['id'].value;
+//   var user = prompt('What is your name?');
+//
+//   if (para == 'player1') {
+//     playerOne.textContent = user;
+//   } else if (para == 'player2') {
+//     playerTwo.textContent = user;
+//   }
+//
+//   if (playerOne.textContent == '') {
+//     alert('Enter a valid name');
+//     playerOne.textContent = 'Player 1 :';
+//   } else if (playerTwo.textContent == '') {
+//     alert('Enter a valid name');
+//     playerTwo.textContent = 'Player 2 :';
+//   }
+// }
+
+// for (var i = 0; i <= 100; i++) {
+//   if (i % 15 == 0) {
+//     console.log('FIZBUZZ');
+//   } else if (i % 3 == 0) {
+//     console.log('FIZZ');
+//   } else if (i % 5 == 0) {
+//     console.log('BUZZ');
+//   } else {
+//     console.log(i);
+//   }
+// }
